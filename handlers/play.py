24ef2847f7299a73358138a58b107399a83ced58
Ihsan•イサン <78595134+Scroxy-X@users.noreply.github.com>
2021-05-08 03:16:24 +0700

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -15,7 +15,7 @@ from helpers.errors import DurationLimitError
 from helpers.gets import get_url, get_file_name
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-@Client.on_message(command("play") & other_filters)
+@Client.on_message(command("putar") & other_filters)
 @errors
 async def play(_, message: Message):
 
@@ -28,7 +28,7 @@ async def play(_, message: Message):
                 [
                     InlineKeyboardButton(
                         text="🔊 Channel",
-                        url="https://t.me/Infinity_BOTs")
+                        url="https://t.me/animechanelindo")
                    
                 ]
             ]
@@ -59,7 +59,7 @@ async def play(_, message: Message):
     else:
         callsmusic.pytgcalls.join_group_call(message.chat.id, file_path)
         await message.reply_photo(
-        photo="https://telegra.ph/file/a4fa687ed647cfef52402.jpg",
+        photo="https://telegra.ph/file/0043272f1ae99cb10b773.jpg",
         reply_markup=keyboard,
         caption="▶️ **Playing** here the song requested by {}!".format(
         message.from_user.mention()
